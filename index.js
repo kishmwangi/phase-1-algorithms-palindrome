7225ee39efb4c0e58e70329d0b448fbbfe2f4ab8
kish mwangi <kishmwangi31@gmail.com>
2024-04-06 21:56:55 +0300

    algorithim

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
+
 function isPalindrome(word) {
   // Write your algorithm here
 }
@@ -20,6 +25,35 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+}
+
+module.exports = isPalindrome;
+function isPalindrome(word) {
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
+}
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
